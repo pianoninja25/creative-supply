2a22a407acb129a4cea9003830265095070bd71c
pianoninjas25 <ymleonards@gmail.com>
2024-06-24 16:25:37 +0700

    Mobile Review 1 _

--- app/ourworks/components/another-service.js
+++ app/ourworks/components/another-service.js
@@ -6,7 +6,10 @@ import { usePathname } from "next/navigation"
 
 const AnotherService = () => {
   const pathname = usePathname() 
-  const [selected, setSelected] = useState(5)
+  const selectPath = pathname.split('/ourworks/')[1].split('_')[0]
+
+
+  const [selected, setSelected] = useState(selectPath)
   
   const boxList = [
     { title: 'Short Video', bg: '5.svg'},
@@ -28,7 +31,7 @@ const AnotherService = () => {
                 ${selected == idx ? 'bg-black/10' : 'bg-black/70'} hover:cursor-pointer hover:bg-black/10`}
                 onClick={() => setSelected(idx)}
               >
-                <Link href={`/ourworks/${idx}_${a.title}`} className={`font-snowfun text-[9vw] text-center leading-10 px-10 ${selected == idx ? 'text-transparent' : 'text-white'}`}>
+                <Link href={`/ourworks/${idx}`} className={`font-snowfun text-[9vw] text-center leading-10 px-10 ${selected == idx ? 'text-transparent' : 'text-white'}`}>
                   {a.title}
                 </Link>
               </div>
@@ -42,7 +45,7 @@ const AnotherService = () => {
 
 
       <div className={`grid justify-center gap-4 py-8 ${pathname.includes('services') ? 'hidden' : ''}`}>
-        <h1 className="text-center font-bold font-montserrat text-3xl text-orange">{boxList[selected].title}</h1>
+        <h1 className="text-center font-bold font-montserrat text-2xl text-orange">{boxList[selected].title}</h1>
         <img src={`/services/${boxList[selected].bg}`} alt="bg" className='w-[20rem]' />
         <p className="w-80 font-montserrat px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
       </div>
